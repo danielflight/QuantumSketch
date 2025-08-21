4d100b5650950f41ed7ad8b3add09b77165d8867
Dan Flight <danielflight@dualityqp.com>
2025-08-21 11:26:52 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="QuantumSketch",
+    version="0.1",
+    packages=find_packages(),
+    # install_requires=[
+        
+    # ]
+    python_requires=">=3.8",
+    author="Daniel Flight",
+    description="Quantum circuit drawing and LaTeX export using qcircuit",
+    url="https://github.com/danielflight/QuantumSketch",
+)
